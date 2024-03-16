974ebc3a7680b206ab3e74dac193cb32e8e02e68
unknown <bthuhang2203@gmail.com>
2024-03-16 16:23:04 +0700

    Responsive header.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Karla: ['Karla', 'sans-serif']
+      },
+      colors:{
+        'light-coffee':'#c89f94'
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        slideDown: 'slideDown .3s ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
-
