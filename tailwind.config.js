4733aeea515d5ddfccf7fd8ba0c9c2072807ac6d
unknown <bthuhang2203@gmail.com>
2024-03-17 18:51:24 +0700

    add story.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
         Karla: ['Karla', 'sans-serif']
       },
       colors:{
-        'light-coffee':'#c89f94'
+        'coffee':{
+          50: '#e8d6d0',
+          200: '#c89f94',
+          400: '#a25f4b',
+          600: '#744838',
+
+        }
       },
       keyframes: {
         slideDown: {
